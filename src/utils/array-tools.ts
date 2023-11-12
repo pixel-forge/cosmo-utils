b44e45fef3f5cdde7409bd26eec987bdac1f3795
Cipher <itayleybovich@gmail.com>
2023-11-12 22:09:47 +0200

    sortArray function

--- src/utils/array-tools.ts
+++ src/utils/array-tools.ts
@@ -5,3 +5,26 @@ export const removeItemFromArray = <T>(arr: T[], instance: T) => {
 
 	arr.splice(index, 1);
 };
+
+export function sortArray<T>(array: T[], map: keyof T | (keyof T)[] | ((item: T) => any) = i => i, invert = false): T[] {
+	const functionMap = map;
+	if (typeof functionMap === 'function') {
+		const compareFn = (a: T, b: T) => {
+			const _a = functionMap(a);
+			const _b = functionMap(b);
+			return (_a < _b ? -1 : (_a === _b ? 0 : 1)) * (invert ? -1 : 1);
+		};
+
+		return array.sort(compareFn);
+	}
+
+	let keys: (keyof T)[];
+	if (!Array.isArray(map))
+		keys = [map as keyof T];
+	else
+		keys = map;
+
+	return keys.reduce((array, key) => {
+		return sortArray<T>(array, item => item[key]);
+	}, array) as T[];
+}
