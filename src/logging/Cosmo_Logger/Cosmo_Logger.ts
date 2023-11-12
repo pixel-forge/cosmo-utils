2c1d1b53bf23b45647d9e01bfe50219b1261a75b
Cipher <itayleybovich@gmail.com>
2023-11-12 21:49:11 +0200

    Cosmo_Logger public log function

--- src/logging/Cosmo_Logger/Cosmo_Logger.ts
+++ src/logging/Cosmo_Logger/Cosmo_Logger.ts
@@ -22,52 +22,10 @@ export class Cosmo_Logger {
 		this.logLevel = logLevel;
 	};
 
-	private log = (logLevel: Cosmo_LogLevel, bold: boolean, toLog: Cosmo_LogParam[]) => {
+	log = (logLevel: Cosmo_LogLevel, bold: boolean, toLog: Cosmo_LogParam[]) => {
 		if (this.logLevel < logLevel)
 			return;
 
 		Cosmo_ClientLogger.log(logLevel, bold, this.tag, toLog);
-	};
-
-	// ################## Class Methods - Log Variations ##################
-
-	logVerbose = (...toLog: Cosmo_LogParam[]) => {
-		this.log(Cosmo_LogLevel.Verbose, false, toLog);
-	};
-
-	logVerboseBold = (...toLog: Cosmo_LogParam[]) => {
-		this.log(Cosmo_LogLevel.Verbose, true, toLog);
-	};
-
-	logDebug = (...toLog: Cosmo_LogParam[]) => {
-		this.log(Cosmo_LogLevel.Debug, false, toLog);
-	};
-
-	logDebugBold = (...toLog: Cosmo_LogParam[]) => {
-		this.log(Cosmo_LogLevel.Debug, true, toLog);
-	};
-
-	logInfo = (...toLog: Cosmo_LogParam[]) => {
-		this.log(Cosmo_LogLevel.Info, false, toLog);
-	};
-
-	logInfoBold = (...toLog: Cosmo_LogParam[]) => {
-		this.log(Cosmo_LogLevel.Info, true, toLog);
-	};
-
-	logWarning = (...toLog: Cosmo_LogParam[]) => {
-		this.log(Cosmo_LogLevel.Warning, false, toLog);
-	};
-
-	logWarningBold = (...toLog: Cosmo_LogParam[]) => {
-		this.log(Cosmo_LogLevel.Warning, true, toLog);
-	};
-
-	logError = (...toLog: Cosmo_LogParam[]) => {
-		this.log(Cosmo_LogLevel.Error, false, toLog);
-	};
-
-	logErrorBold = (...toLog: Cosmo_LogParam[]) => {
-		this.log(Cosmo_LogLevel.Error, true, toLog);
 	};
 }
