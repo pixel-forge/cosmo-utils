f640e8150eaec93a6df3b49eb9a6d034dbfe1dfe
Cipher <itayleybovich@gmail.com>
2023-11-12 22:54:40 +0200

    exporting

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@
 export * from './logging/Cosmo_Logger';
 
 //Utils
+export * from './utils/general-tools'
 export * from './utils/object-tools';
 export * from './utils/array-tools';
+export * from './utils/string-tools';
