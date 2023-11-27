2003d28e6b9017c21ef7d5c3c065c7b8d0fe80e7
Cipher <itayleybovich@gmail.com>
2023-11-27 22:02:09 +0200

    re-structuring, module manager

--- src/modules/base/Cosmo_ModuleManager.ts
+++ src/modules/base/Cosmo_ModuleManager.ts
@@ -1,4 +1,4 @@
-import {Cosmo_Logger, Cosmo_LogLevel} from '../../logging/Cosmo_Logger';
+import {Cosmo_Logger, Cosmo_LogLevel, Cosmo_LogParam} from '../../logging/Cosmo_Logger';
 import {Cosmo_Module} from './Cosmo_Module';
 import {exists} from '../../utils/general-tools';
 
@@ -19,36 +19,33 @@ export abstract class Cosmo_ModuleManager
 
 	public init = () => {
 		this.setMinLevel(this.config?.logLevel ?? Cosmo_LogLevel.Info);
-		this.log(Cosmo_LogLevel.Info, false, ['Initializing App']);
+		this.logInfo('Initializing App');
 
 		//Validate modules
 		const invalidModules = this.getModules(module => !exists(module));
 		if (invalidModules.length) {
-			this.log(Cosmo_LogLevel.Error, true, [
+			this.logErrorBold(
 				'Modules came back as undefined, check your imports!',
 				'List of modules:',
 				...this.modules.map(module => module.getTag() ?? 'undefined'),
-			]);
+			);
 			throw new Error('Failed to validate modules');
 		}
 
 		//Set module configs
-		this.log(Cosmo_LogLevel.Info, false, ['Setting Module Configs']);
+		this.logInfo('Setting Module Configs');
 		this.modules.forEach(module => {
 			module.setConfig(this.config[module.getTag()]);
 		});
 
 		//Initialize modules
-		this.log(Cosmo_LogLevel.Info, false, ['Initializing Modules']);
+		this.logInfo('Initializing Modules');
 		this.modules.forEach(module => {
 			try {
 				//@ts-ignore - this is the one place to call init on modules
 				module.init();
 			} catch (error: any) {
-				this.log(Cosmo_LogLevel.Error, true, [
-					`Failed to initiate module ${module.getTag()}`,
-					error
-				]);
+				this.logErrorBold(`Failed to initiate module ${module.getTag()}`, error);
 			}
 		});
 	};
@@ -65,5 +62,47 @@ export abstract class Cosmo_ModuleManager
 
 	public getModules = (filter: (module: Cosmo_Module) => boolean = () => true) => {
 		return this.modules.filter(filter);
+	};
+
+	// ################## Class Logic - Logging ##################
+
+	protected logVerbose = (...toLog: Cosmo_LogParam[]) => {
+		this.log(Cosmo_LogLevel.Verbose, false, toLog);
+	};
+
+	protected logVerboseBold = (...toLog: Cosmo_LogParam[]) => {
+		this.log(Cosmo_LogLevel.Verbose, true, toLog);
+	};
+
+	protected logDebug = (...toLog: Cosmo_LogParam[]) => {
+		this.log(Cosmo_LogLevel.Debug, false, toLog);
+	};
+
+	protected logDebugBold = (...toLog: Cosmo_LogParam[]) => {
+		this.log(Cosmo_LogLevel.Debug, true, toLog);
+	};
+
+	protected logInfo = (...toLog: Cosmo_LogParam[]) => {
+		this.log(Cosmo_LogLevel.Info, false, toLog);
+	};
+
+	protected logInfoBold = (...toLog: Cosmo_LogParam[]) => {
+		this.log(Cosmo_LogLevel.Info, true, toLog);
+	};
+
+	protected logWarning = (...toLog: Cosmo_LogParam[]) => {
+		this.log(Cosmo_LogLevel.Warning, false, toLog);
+	};
+
+	protected logWarningBold = (...toLog: Cosmo_LogParam[]) => {
+		this.log(Cosmo_LogLevel.Warning, true, toLog);
+	};
+
+	protected logError = (...toLog: Cosmo_LogParam[]) => {
+		this.log(Cosmo_LogLevel.Error, false, toLog);
+	};
+
+	protected logErrorBold = (...toLog: Cosmo_LogParam[]) => {
+		this.log(Cosmo_LogLevel.Error, true, toLog);
 	};
 }
